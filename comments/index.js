fec5507bf1f95cf5d100e179090abf283b5ce68c
Rafael Augusto <rafaelaugusto.developer@gmail.com>
2020-05-07 07:56:11 -0300

    handling moderation

--- comments/index.js
+++ comments/index.js
@@ -29,8 +29,28 @@ app.post('/posts/:id/comments', async (req, res) => {
   res.status(201).send(comments)
 })
 
-app.post('/events', (req, res) => {
+app.post('/events', async (req, res) => {
   console.log('Event received:', req.body.type)
+
+  const { type, data } = req.body
+
+  if(type === 'CommentModerated'){
+    const { postId, id, status, content } = data
+  
+    const comments = commentsByPostId[postId]
+    const comment = comments.find(comment => comment.id === id)
+    comment.status = status
+
+    await axios.post('http://localhost:4005', {
+      type: 'CommentUpdated',
+      data: {
+        id,
+        status,
+        postId,
+        content
+      }
+    })
+  }
 
   res.send({})
 })
