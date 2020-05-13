ff672bf9440af61db3892a58dada8aa699bc8244
Rafael Augusto <rafaelaugusto.developer@gmail.com>
2020-05-13 18:17:40 -0300

    edit dockerfile client

--- comments/index.js
+++ comments/index.js
@@ -24,7 +24,7 @@ app.post('/posts/:id/comments', async (req, res) => {
 
   commentsByPostId[req.params.id] = comments
 
-  await axios.post('http://localhost:4005/events', { type: 'CommentCreated', data: { id: commentId, content, postId: req.params.id, status: 'pending' } })
+  await axios.post('http://event-bus-srv:4005/events', { type: 'CommentCreated', data: { id: commentId, content, postId: req.params.id, status: 'pending' } })
 
   res.status(201).send(comments)
 })
@@ -41,7 +41,7 @@ app.post('/events', async (req, res) => {
     const comment = comments.find(comment => comment.id === id)
     comment.status = status
 
-    await axios.post('http://localhost:4005', {
+    await axios.post('http://event-bus-srv:4005', {
       type: 'CommentUpdated',
       data: {
         id,
